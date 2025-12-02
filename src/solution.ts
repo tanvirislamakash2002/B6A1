241a8646c7e8621ccf8257e6312d3268fc065fad
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-02 20:39:32 +0600

    solved problem 5 filter active user

--- src/solution.ts
+++ src/solution.ts
@@ -62,6 +62,29 @@ const books = [
 // console.log(filterByRating(books));
 
 
+// problem 5-------------------------------------------------------
+
+type User = { id: number, name: string, email: string, isActive: boolean }[]
+const filterActiveUsers = (users: User): User => {
+    return users.filter(user => user.isActive === true)
+}
+
+const users = [
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+
+// console.log(filterActiveUsers(users));
+
+
+
+
+
+
+
+
+
 
 
 
