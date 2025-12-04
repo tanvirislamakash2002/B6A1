5cf61b5270716acdfa421405f0ad08e7fe554e9a
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-12-04 14:54:40 +0600

    blog writing done of enum question

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # What is the use of enums in TypeScript? Provide an example of a numeric and string enum.
 
+ enum হল একটা type যাকে আমরা value হিসেবে ব্যবহার করতে পারি. এটি মূলত এক হচ্ছে string literal  কে এক জায়গায় রাখে.
+
+উদাহরণ হিসেবে আমরা যদি string literal কে type হিসেবে ব্যবহার করে যেমন
+
+type Roles = “Admin”|”Manager”|”Employee”
+
+তাহলে আমরা এটিকে ফাংশনের মধ্যে শুধু টাইপ হিসেবেই ব্যবহার করতে পারব, যেমন-
+
+const functionName =(role: Roles)⇒{….
+
+কিন্তু এটিকে আমরা value হিসেবে use করতে পারবো না. এর ফলে যদি আমরা ফাংশনের মধ্যে condition use করি তাহলে ভুল হওয়ার সম্ভাবনা থাকে, যেমন-
+
+const functionName =(role:Roles)⇒{
+
+if(role===”Addmin”){…..
+
+যদি আমরা এনাম ইউজ করি তাহলে এ ধরনের ভুল হওয়ার সম্ভাবনা অনেকটাই কমে যায়. কারণ ইমাম একই সাথে টাইপ এবং ভ্যালু কিভাবে ইউজ ব্যবহার করা যায়.
+
+enum Roles {
+
+Admin = “Admin”,
+
+Manager=”Manager”,
+
+Employee=”Employee”
+
+}
+
+এখন যদি আমরা এটিকে ফাংশন এবং কন্ডিশন এর মধ্যে ব্যবহার করি তাহলে, ভুল হওয়ার সম্ভাবনা অনেকটাই কমে যায় যেমন
+
+const functionName =(role:Roles)⇒{
+
+if(role===Roles.Addmin){…..
+
+বি:দ্র:- ইনাম ফাইটার বান্ডেল সাইজ অনেক বড় করে দেয় তাই এদিকে বেশি ব্যবহার করা উচিত নয়
+
+**example of a numeric and string enum**
+
+string enum-
+
+enum Roles {
+
+Admin = “Admin”,
+
+Manager=”Manager”,
+
+Employee=”Employee”
+
+}
+
+numeric enum-
+
+enum Position{
+
+first,
+
+second,
+
+third
+
+}
+
+নিউমেরিক এনাম অটোমেটিক্যালি ভ্যালুগুলোকে নাম্বারে রূপান্তরিত করে, যেমন-
+
+first = 0, second = 1, third=2
--- src/solution.ts
+++ src/solution.ts
@@ -1,8 +1,7 @@
-// problem 1 -------------------------------------------------------
 
 type FormatValue = string | number | boolean
 
-const formatValue = (input1: FormatValue):FormatValue => {
+const formatValue = (input1: FormatValue): FormatValue => {
     if (typeof input1 === 'string') {
         return input1.toUpperCase()
     }
@@ -11,29 +10,19 @@ const formatValue = (input1: FormatValue):FormatValue => {
     }
     else if (typeof input1 === 'boolean') {
         return !input1
-    }else{
-        return "The Type is invalid"
     }
+    throw new Error('Invalid Input');
 
 }
-// console.log(formatValue('sss'));
 
-
-// problem 2-------------------------------------------------------
-const getLength = (value2: (string | any[])):number => {
-    if(typeof value2 === 'string'){
-        return value2.length
+const getLength = (input2: (string | any[])): number => {
+    if (typeof input2 === 'string' || Array.isArray(input2)) {
+        return input2.length
     }
-    else if(Array.isArray(value2)){
-        return value2.length
-    }else{
-        return 0
-    }
+    throw new Error('Invalid Input');
 }
 
-console.log(getLength([1,6,9,12]));
 
-// problem 3-------------------------------------------------------
 
 class Person {
     name: string;
@@ -44,94 +33,95 @@ class Person {
         this.age = age
     }
     getDetails(): string {
-        return `Name: ${this.name} , Age: ${this.age} `
+        return `Name: ${this.name}, Age: ${this.age} `
     }
 }
 
-const person1 = new Person('akash', 30);
 
-// console.log(person1.getDetails());
 
-// problem 4-------------------------------------------------------
 
-type FilterByRating = { title: string, rating: number }[]
 
-const filterByRating = (books: FilterByRating): FilterByRating => {
-    return books.filter(book => book.rating >= 4)
+type FilterByRating = { title: string, rating: number }
+
+const filterByRating = (input3: FilterByRating[]): FilterByRating[] => {
+    return input3.filter(i => i.rating >= 4)
 }
 
 
-const books = [
-    { title: 'Book A', rating: 4.5 },
-    { title: 'Book B', rating: 3.2 },
-    { title: 'Book B', rating: 4.2 },
-    { title: 'Book C', rating: 5.0 },
-];
-
-// console.log(filterByRating(books));
 
 
-// problem 5-------------------------------------------------------
 
-type User = { id: number, name: string, email: string, isActive: boolean }[]
-const filterActiveUsers = (users: User): User => {
+
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    isActive: boolean
+}
+
+const filterActiveUsers = (users: User[]): User[] => {
     return users.filter(user => user.isActive === true)
 }
 
-const users = [
-    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
-
-// console.log(filterActiveUsers(users));
 
 
-// problem 6-------------------------------------------------------
-
-type Book = {
-    title: string,
-    author: string,
-    publishedYear: number
-    isAvailable: boolean
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
 }
 
 
-const printBookDetails = (input6: Book) => {
-    const {
-        title, author, publishedYear, isAvailable, } = input6
-    return console.log(`Title: ${title}, Author: ${author}, Published: ${publishedYear}, Available: ${isAvailable ? 'Yes' : 'No'}`);
+const printBookDetails = (book: Book): void => {
+    const { title, author, publishedYear, isAvailable } = book
+
+    console.log(`Title: ${title}, Author: ${author}, Published: ${publishedYear}, Available: ${isAvailable ? 'Yes' : 'No'}`);
 }
 
 const myBook: Book = {
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
     publishedYear: 1925,
-    isAvailable: true,
+    isAvailable: false,
 };
 
-// printBookDetails(myBook);
+
+type GetUniqueValues = string | number
+
+const getUniqueValues = (array1: GetUniqueValues[], array2: GetUniqueValues[]): GetUniqueValues[] => {
+
+    const uniqueValues: GetUniqueValues[] = []
 
 
-// problem 7-------------------------------------------------------
 
-const getUniqueValues = (array1: number[], array2: number[]): number[] => {
-    const uniqueValues = array1
-    array2.forEach(value => {
-        if (!uniqueValues.includes(value)) {
-            uniqueValues.push(value)
+    const isAvailable = (array: GetUniqueValues[], value: GetUniqueValues): boolean => {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                return true;
+            }
         }
-    })
+        return false
+    }
+
+    for (let i = 0; i < array1.length; i++) {
+        if (!isAvailable(uniqueValues, array1[i])) {
+
+            uniqueValues.push(array1[i])
+        }
+    }
+
+    for (let i = 0; i < array2.length; i++) {
+        if (!isAvailable(uniqueValues, array2[i])) {
+
+            uniqueValues.push(array2[i])
+        }
+    }
     return uniqueValues
 }
 
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-// console.log(getUniqueValues(array1, array2));
 
-
-// problem 8-------------------------------------------------------
 
 type CalculateTotalPrice = {
     name: string,
@@ -139,23 +129,26 @@ type CalculateTotalPrice = {
     quantity: number
     discount?: number
 }[]
+
+
 const calculateTotalPrice = (products: CalculateTotalPrice): number => {
+
     let totalPrice = 0
+
     products.forEach(product => {
-        const productPrice=product.price* product.quantity
-        const discountPrice = product.discount?productPrice*(1-(product.discount/100)) : productPrice
-        totalPrice += discountPrice 
+
+        const productPrice = product.price * product.quantity
+
+        const discountPrice = product.discount ? productPrice * (1 - (product.discount / 100)) : productPrice
+
+        totalPrice += discountPrice
+
     })
+
     return totalPrice
 
 }
-const products = [
-    { name: 'Pen', price: 10, quantity: 2 },
-    { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-    { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
 
-// console.log(calculateTotalPrice(products));
 
 
 
